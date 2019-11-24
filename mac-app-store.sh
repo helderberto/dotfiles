30d1dd6087e9598dda2bb20765beddb5f15dabd7
Helder Burato Berto <helder.burato@gmail.com>
2019-11-24 11:13:26 -0300

    added: mac app store config

---
+++ mac-app-store.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Mac App Store installation...
+
+mas 'Todoist', id: 585829637
+mas 'Bear', id: 1091189122
+mas 'The Unarchiver', id: 425424353
+mas 'Amphetamine', id: 937984704
+mas 'Giphy Capture', id: 668208984
+mas 'Flycut (Clipboard manager)', id: 442160987
