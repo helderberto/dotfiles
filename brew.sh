b9b462839f1398552c0051b99e88331f5566dcbd
Helder Burato Berto <helder.burato@gmail.com>
2019-11-24 11:13:16 -0300

    added: brew installations

---
+++ brew.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+# GNU `sed`, overwriting the built-in `sed`
+brew install gnu-sed --with-default-names
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Install more recent versions of some OS X tools
+brew install vim --with-override-system-vi
+brew install homebrew/dupes/nano
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+# z hopping around folders
+brew install z
+
+brew install git
+brew install tree
+brew install zsh
+brew install antigen
+brew install node
+brew install yarn --ignore-dependencies
+
+# Remove outdated versions from the cellar
+brew cleanup
