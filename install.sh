804710ee217a8d26719a6f8e6b41c867ee70a878
Helder Burato Berto <helder.burato@gmail.com>
2019-11-24 11:12:57 -0300

    updated: install.sh config

--- install.sh
+++ install.sh
@@ -7,27 +7,16 @@ if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Update Homebrew recipes
-brew update
-
-# Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle
-
-curl -L git.io/antigen > antigen.zsh
-# or use git.io/antigen-nightly for the latest version
-
 # Create a Workspace directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
 mkdir $HOME/Workspace
 mkdir $HOME/Workspace/labs
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+# Removes .zshrc from $HOME (if it exists)
 rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+# Create symlinks the .zshrc file from the .dotfiles
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
