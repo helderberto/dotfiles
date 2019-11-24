ce8081471b7368e0a8d7ed9a42c932100be5f8da
Helder Burato Berto <helder.burato@gmail.com>
2019-11-24 11:46:16 -0300

    updated: brew.sh removed dupes deprecated

--- brew.sh
+++ brew.sh
@@ -20,13 +20,6 @@ brew install gnu-sed --with-default-names
 brew tap homebrew/bundle
 brew bundle
 
-# Install more recent versions of some OS X tools
-brew install vim --with-override-system-vi
-brew install homebrew/dupes/nano
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-
 # z hopping around folders
 brew install z
 
