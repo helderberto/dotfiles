5af919c99b76993ed9045ecb098836d7a1da136f
Helder Burato Berto <helder.burato@gmail.com>
2019-11-24 22:03:29 -0300

    refactor: mac installs moved to mac folder

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-echo "Setting up your Mac..."
+echo "🔧 Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-source brew.sh
-source brew-cask.sh
-source mac-app-store.sh
+source $HOME/homebrew/brew.sh
+source $HOME/homebrew/brew-cask.sh
+source $HOME/mac/app-store.sh
 
 # Remove Workspace
 rm -rf $HOME/Workspace
@@ -26,4 +26,7 @@ source $HOME/.zshrc
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
-source .macos
+source $HOME/mac/.macos
+
+# Add default apps to Dock
+source $HOME/mac/dock.sh
