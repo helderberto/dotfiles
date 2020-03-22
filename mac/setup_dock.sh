19e3cbdf6dfa4c2741afb81b1a1a15d91aa16b25
Helder Burato Berto <helder.burato@gmail.com>
2020-03-22 17:45:18 -0300

    mac(setup_dock): remove Insomnia and add MindNode

--- mac/setup_dock.sh
+++ mac/setup_dock.sh
@@ -12,8 +12,8 @@ add_app_to_dock "iTerm"
 add_app_to_dock "Todoist"
 add_app_to_dock "Bear"
 add_app_to_dock "Slack"
+add_app_to_dock "MindNode"
 add_app_to_dock "Spotify"
-add_app_to_dock "Insomnia"
 add_folder_to_dock $HOME/Downloads -d 0 -s 2 -v 1
 
 killall Dock
