80e5dac76d1e55d6fa367afc9a65d8ce39055b27
Helder Burato Berto <helder.burato@gmail.com>
2020-03-22 17:43:43 -0300

    mac(app_store): link to MindNode download

--- mac/app_store.sh
+++ mac/app_store.sh
@@ -2,9 +2,10 @@
 
 # Mac App Store installation...
 
-mas install 585829637 # Todoist 
-mas install 1091189122 # Bear
-mas install 425424353  # The Unarchiver 
-mas install 937984704 # Amphetamine 
-mas install 668208984 # Giphy Capture 
-mas install 442160987 # Flycut (Clipboard manager) 
+mas install 585829637       # Todoist
+mas install 1091189122      # Bear
+mas install 1289197285      # MindNode
+mas install 425424353       # The Unarchiver
+mas install 937984704       # Amphetamine
+mas install 668208984       # Giphy Capture
+mas install 442160987       # Flycut (Clipboard manager)
