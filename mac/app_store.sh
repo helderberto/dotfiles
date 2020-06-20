9577b3435180d398516494c1f615ed9e341ee697
Helder Burato Berto <helder.burato@gmail.com>
2020-06-20 14:36:44 -0300

    mac: remove Bear download from mac app store

--- mac/app_store.sh
+++ mac/app_store.sh
@@ -3,7 +3,6 @@
 # Mac App Store installation...
 
 mas install 585829637       # Todoist
-mas install 1091189122      # Bear
 mas install 425424353       # The Unarchiver
 mas install 937984704       # Amphetamine
 mas install 668208984       # Giphy Capture
