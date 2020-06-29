4eff5fc06a293ca40593829d3b18d82ece45cbd3
Helder Burato Berto <helder.burato@gmail.com>
2020-06-29 13:53:05 -0300

    mac: add Bear to install and mac dock

--- mac/app_store.sh
+++ mac/app_store.sh
@@ -3,6 +3,7 @@
 # Mac App Store installation...
 
 mas install 585829637       # Todoist
+mas install 1091189122      # Bear
 mas install 425424353       # The Unarchiver
 mas install 937984704       # Amphetamine
 mas install 668208984       # Giphy Capture
