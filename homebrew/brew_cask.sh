8372e755f8169353d772319c5f2c66ad6273562d
Helder Burato Berto <helder.burato@gmail.com>
2020-07-24 18:15:35 -0300

    brew: change iterm2 to alacritty

--- homebrew/brew_cask.sh
+++ homebrew/brew_cask.sh
@@ -13,7 +13,7 @@ brew cask install notion
 brew cask install pomodone
 
 # dev
-brew cask install iterm2
+brew cask install alacritty
 brew cask install visual-studio-code
 brew cask install insomnia
 brew cask install imageoptim
@@ -26,7 +26,7 @@ brew cask install slack
 
 # fonts
 brew tap homebrew/cask-fonts
-brew cask install font-fira-code
 brew cask install font-hack
+brew cask install font-fira-code
 brew cask install font-open-sans
 brew cask install font-roboto
