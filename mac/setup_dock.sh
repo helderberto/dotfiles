4890af7a17e8089164d843962b2fd5a6e9a43310
Helder Burato Berto <helder.burato@gmail.com>
2020-08-04 16:51:42 -0300

    mac: Remove Todoist and Bear from dock

--- mac/setup_dock.sh
+++ mac/setup_dock.sh
@@ -8,9 +8,8 @@ disable_recent_apps_from_dock
 
 add_app_to_dock "Google Chrome"
 add_app_to_dock "Emacs"
+add_app_to_dock "Alacritty"
 add_app_to_dock "iTerm"
-add_app_to_dock "Todoist"
-add_app_to_dock "Bear"
 add_app_to_dock "Slack"
 add_app_to_dock "Spotify"
 add_folder_to_dock $HOME/Downloads -d 0 -s 2 -v 1
