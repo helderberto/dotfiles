d338f58d1256c698addda5257d18b8cca79418d6
Helder Burato Berto <helder.burato@gmail.com>
2020-08-19 20:35:43 -0300

    emacs: Enable go mode

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -123,7 +123,7 @@
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       ;;(go +lsp)         ; the hipster dialect
+       (go +lsp)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
