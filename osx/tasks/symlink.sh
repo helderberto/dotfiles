c2f83babce851391386bbeea58e2f4374a232173
Helder Burato Berto <helder.burato@gmail.com>
2020-08-20 19:36:26 -0300

    osx(symlink): Fix the dotfile links

--- osx/tasks/symlink.sh
+++ osx/tasks/symlink.sh
@@ -5,14 +5,16 @@ DOTFILE_DIR=$HOME/.dotfiles
 DOTFILES=(
 	"zsh"
 	"bash"
-	"git",
-	"alacritty",
-	"tmux",
-	"emacs",
+	"git"
+	"alacritty"
+	"tmux"
+	"emacs"
     "vim"
 )
 
+cd $DOTFILE_DIR
+
 for dotfile in "${DOTFILES[@]}";do
-	echo "🔗 Linking dotfile: $HOME/.${dotfile}"
+	echo "🔗 Linking dotfile: $HOME/${dotfile}"
 	stow $dotfile
 done
