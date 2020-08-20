7686e9396dae94427f0379f6889e0c576aa8fb5f
Helder Burato Berto <helder.burato@gmail.com>
2020-08-20 08:11:18 -0300

    osx(dock): Remove iTerm

--- osx/dock/setup.sh
+++ osx/dock/setup.sh
@@ -9,7 +9,6 @@ disable_recent_apps_from_dock
 add_app_to_dock "Firefox Developer Edition"
 add_app_to_dock "Emacs"
 add_app_to_dock "Alacritty"
-add_app_to_dock "iTerm"
 add_app_to_dock "Slack"
 add_app_to_dock "Spotify"
 add_folder_to_dock $HOME/Downloads -d 0 -s 2 -v 1
