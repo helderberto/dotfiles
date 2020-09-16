5ff81710c1a4feb1245e1c64845eff487d10ebae
Helder Burato Berto <helder.burato@gmail.com>
2020-09-16 17:37:14 -0300

    osx(tasks): Add download NVM via curl

--- osx/tasks/nvm_setup.sh
+++ osx/tasks/nvm_setup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir ~/.nvm
+# Download and install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# Install LTS version
 nvm install --lts
-nvm use node
+nvm use --lts 
 nvm run node --version
 
+## Solve issue zsh compinit: insecure directories and files, run compaudit for list.
+compaudit | xargs chmod g-w
