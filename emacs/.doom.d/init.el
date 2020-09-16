f9f5ec10590ad312f367d7a6682b1ce18f10afc6
Helder Burato Berto <helder.burato@gmail.com>
2020-09-16 17:38:31 -0300

    emacs: enable clojure on init.el

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -109,7 +109,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;cc                ; C/C++/Obj-C madness
-       ;;clojure           ; java with a lisp
+       clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
@@ -142,7 +142,7 @@
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
-       ;;ocaml             ; an objective camel
+       ;; ocaml             ; an objective camel
        (org +pomodoro +journal +hugo)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
