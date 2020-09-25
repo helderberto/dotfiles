24f019f7993a85d6043cba8cfff0d95d92d72aac
Helder Burato Berto <helder.burato@gmail.com>
2020-09-25 16:30:32 -0300

    emacs(init): enable ivy +fuzzy
    Improves the ivy history search

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -22,7 +22,7 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +fuzzy)        ; a search engine for love and life
 
        :ui
        deft              ; notational velocity for Emacs
