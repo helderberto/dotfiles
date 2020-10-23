1e1d98b2ea3302b17958732e2b656875f51dc9a1
Helder Burato Berto <helder.burato@gmail.com>
2020-10-23 14:54:26 -0300

    emacs(init): enable multiple-cursors

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -56,7 +56,7 @@
        (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       ;;multiple-cursors  ; editing in many places at once
+       multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
