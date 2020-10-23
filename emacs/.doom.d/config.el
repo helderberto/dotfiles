3647c860b11b63867be53886f41bfaa27176cc7c
Helder Burato Berto <helder.burato@gmail.com>
2020-10-23 14:54:05 -0300

    emacs(config): add prettier-js hook

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -158,3 +158,23 @@
   :init
   (setq rmh-elfeed-org-files (list "~/org/references/readlater.org")))
 
+(use-package! prettier-js
+  :after js2-mode
+  :init
+  (add-hook! 'js2-mode-hook 'prettier-js-mode)
+  (add-hook! 'web-mode-hook 'prettier-js-mode)
+  :config
+  (setq prettier-js-args '(
+                            "--trailing-comma" "all"
+                            "--bracket-spacing" "true"
+                            ))
+
+  (defun enable-minor-mode (my-pair)
+    "Enable minor mode if filename match the regexp.  MY-PAIR is a cons cell (regexp . minor-mode)."
+    (if (buffer-file-name)
+      (if (string-match (car my-pair) buffer-file-name)
+        (funcall (cdr my-pair)))))
+
+  (add-hook! 'web-mode-hook #'(lambda ()
+                                (enable-minor-mode
+                                  '("\\.jsx?\\'" . prettier-js-mode)))))
