b70d62c65e31a92b01e11b7ee5381477cc0ec7f2
Helder Burato Berto <helder.burato@gmail.com>
2020-12-16 23:53:16 -0300

    osx(tasks): update brew_setup.sh order execution

--- osx/tasks/brew_setup.sh
+++ osx/tasks/brew_setup.sh
@@ -2,14 +2,14 @@
 
 # Install command-line tools using Homebrew.
 
+# It will install all from Brewfile
+brew bundle 
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
 brew upgrade
-
-# it will install all from Brewfile
-brew bundle install
 
 # Remove outdated versions from the cellar
 brew cleanup
