40c0701cd1ba21a5a09c961514ac802b35f95f3d
Helder Burato Berto <helder.burato@gmail.com>
2020-12-19 17:56:03 -0300

    Create mas_setup.sh

---
+++ osx/tasks/mas_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mas install 585829637 // Todoist
+mas install 425424353 // The Unarchiver
+mas install 442160987 // Flycut
+mas install 937984704 // Amphetamine
+mas install 668208984 // GIPHY CAPTURE
