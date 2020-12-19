3073ab695d74e2578ecbb4dd611b3c94a0484dd6
Helder Burato Berto <helder.burato@gmail.com>
2020-12-19 15:36:15 -0300

    Update brew_setup.sh

--- osx/tasks/brew_setup.sh
+++ osx/tasks/brew_setup.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+# Give required permissions to Homebrew
+sudo chown -R $(whoami):admin /usr/local && sudo chmod -R g+rwx /usr/local
+
 # Install command-line tools using Homebrew.
+
+# Remove outdated versions from the cellar
+brew cleanup
 
 # Make sure we’re using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
 brew upgrade
-
-# Remove outdated versions from the cellar
-brew cleanup
 
 # It will install all from Brewfile
 brew bundle --file=~/.dotfiles/osx/tasks/Brewfile
