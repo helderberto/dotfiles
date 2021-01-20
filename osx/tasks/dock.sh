a745cd7a283da96d1356dcd55d1b9a0fc30c6e25
Helder Burato Berto <helder.burato@gmail.com>
2021-01-20 12:40:51 +0000

    osx(dock): add Notion

--- osx/tasks/dock.sh
+++ osx/tasks/dock.sh
@@ -10,6 +10,7 @@ add_app_to_dock "Google Chrome"
 add_app_to_dock "Emacs"
 add_app_to_dock "Alacritty"
 add_app_to_dock "Todoist"
+add_app_to_dock "Notion"
 add_app_to_dock "Slack"
 add_app_to_dock "Spotify"
 add_folder_to_dock $HOME/Downloads -d 0 -s 2 -v 1
