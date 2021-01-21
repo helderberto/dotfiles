6ccb4ffa0a78496a4e08e26952fb31cefb9e99f8
Helder Burato Berto <helder.burato@gmail.com>
2021-01-21 11:34:14 +0000

    emacs(config): set org-roam directory

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -36,6 +36,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+(setq org-roam-directory "~/org/roam")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
