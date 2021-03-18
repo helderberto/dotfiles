09c8aa4d1b67cd3c5e72f7735fac51483d81fc65
Helder Burato Berto <helder.burato@gmail.com>
2021-03-18 10:35:26 +0000

    osx(brew): add Visual Studio Code

--- osx/tasks/brew.sh
+++ osx/tasks/brew.sh
@@ -69,6 +69,7 @@ brew install bitwarden-cli
 # Install cask packages
 brew install --cask alacritty
 brew install --cask emacs
+brew install --cask visual-studio-code
 brew install --cask figma
 brew install --cask google-chrome
 brew install --cask imageoptim
