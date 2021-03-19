f9215974e6aeb4e08564c9a0d5a778b30ffbcba5
Helder Burato Berto <helder.burato@gmail.com>
2021-03-19 09:54:24 +0000

    common(doom_emacs): remove echo's from file

--- common/doom_emacs_setup.sh
+++ common/doom_emacs_setup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-echo "ℰ Installing Doom Emacs"
+# DOOM Emacs clone and install
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
-echo "✅ Successful installed Doom Emacs"
