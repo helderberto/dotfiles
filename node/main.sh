6110494d26004f61acad130a0c42600b1f2c467e
Helder Burato Berto <helder.burato@gmail.com>
2021-04-17 10:53:12 +0100

    node: fix nvm node link to /usr/local/Cellar

--- node/main.sh
+++ node/main.sh
@@ -11,10 +11,20 @@ else
 
     ## Install nvm --lts and set as default
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+    ## Install and use --lts version
     nvm install --lts
-    nvm use default
+    nvm use --lts
+    nvm alias default node
+
+    ## Solve problem link node from nvm to brew for yarn usage
+    ## https://github.com/nijicha/install_nodejs_and_yarn_homebrew
+    mkdir /usr/local/Cellar/node
+    ln -s ~/.nvm/versions/node/$(nvm current)/ /usr/local/Cellar/node
+    brew link --overwrite node
 
     ## Solve issue zsh compinit: insecure directories and files, run compaudit for list.
     compaudit | xargs chmod g-w
+
     msg_ok "OK"
 fi
