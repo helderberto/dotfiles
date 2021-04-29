e14d6552d4e13c57e385df2adedb11f058fd0361
Helder Burato Berto <helder.burato@gmail.com>
2021-04-29 09:56:54 +0100

    emacs(init): disable spell, org +pretty, +journal

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -81,7 +81,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       spell             ; tasing you for misspelling mispelling
+       ;; spell             ; tasing you for misspelling mispelling
        ;; grammar           ; tasing grammar mistake every you make
 
        :tools
@@ -143,9 +143,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;; ocaml             ; an objective camel
-        (org
-          +pretty
-          +journal)
+       org
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
