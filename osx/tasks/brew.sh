913889080605d1f5996cdcc44912e6444f36f41d
Helder Burato Berto <helder.burato@gmail.com>
2021-12-22 09:47:56 +0000

    osx(brew): remove bluejeans, dozer, and drawio

--- osx/tasks/brew.sh
+++ osx/tasks/brew.sh
@@ -83,11 +83,8 @@ brew install --cask slack
 brew install --cask discord
 brew install --cask spotify
 brew install --cask bitwarden
-brew install --cask bluejeans
-brew install --cask dozer
 brew install --cask grammarly
 brew install --cask rectangle
-brew install --cask drawio
 brew install --cask visual-studio-code
 brew install --cask obsidian
 
