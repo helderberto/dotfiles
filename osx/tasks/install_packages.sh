efb53e8385c4e0ba77027723845d836717a144b6
Helder Burato Berto <helder.burato@gmail.com>
2022-01-27 16:03:34 +0000

    osx(tasks): move NVM install to install_packages.sh

--- osx/tasks/install_packages.sh
+++ osx/tasks/install_packages.sh
@@ -53,4 +53,31 @@ else
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     msg_ok "OK"
-f
+fi
+
+# Download and install NVM
+if [ -d "$HOME/.nvm" ]; then
+    msg_checking "Node Version Manager"
+else
+    msg_install "Node Version Manager" "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+    ## Install nvm --lts and set as default
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+    ## Install and use --lts version
+    nvm install --lts
+    nvm use --lts
+    nvm alias default node
+
+    ## Solve problem link node from nvm to brew for yarn usage
+    ## https://github.com/nijicha/install_nodejs_and_yarn_homebrew
+    mkdir /usr/local/Cellar/node
+    ln -s ~/.nvm/versions/node/$(nvm current)/ /usr/local/Cellar/node
+    brew link --overwrite node
+
+    ## Solve issue zsh compinit: insecure directories and files, run compaudit for list.
+    compaudit | xargs chmod g-w
+
+    msg_ok "OK"
+fi
