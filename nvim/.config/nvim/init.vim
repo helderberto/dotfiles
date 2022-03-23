f927e53d004c173a932b572a09a1032f60c6def4
Helder Burato Berto <helder.burato@gmail.com>
2022-03-23 16:13:44 +0000

    neovim: add bufferline

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -71,6 +71,7 @@ Plug 'SirVer/ultisnips'
 Plug 'helderburato/aragorn-vim-snippets'
 Plug 'jiangmiao/auto-pairs'
 Plug 'kyazdani42/nvim-tree.lua'
+Plug 'akinsho/bufferline.nvim'
 
 " Initialize plugin system
 call plug#end()
@@ -137,6 +138,18 @@ nnoremap <C-b> :Buffers<CR>
 nnoremap <leader>b :Buffers<CR>
 nnoremap <leader>bd :bdelete!<CR>
 nnoremap <leader>h :History<CR>
+" These commands will navigate through buffers in order regardless of which mode you are using
+" e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
+nnoremap <silent>L :BufferLineCycleNext<CR>
+nnoremap <silent>H :BufferLineCyclePrev<CR>
+" These commands will move the current buffer backwards or forwards in the bufferline
+nnoremap <silent><leader>bn :BufferLineMoveNext<CR>
+nnoremap <silent><leader>bp :BufferLineMovePrev<CR>
+
+" These commands will sort buffers by directory, language, or a custom criteria
+nnoremap <silent>be :BufferLineSortByExtension<CR>
+nnoremap <silent>bd :BufferLineSortByDirectory<CR>
+nnoremap <silent>bs :lua require'bufferline'.sort_buffers_by(function (buf_a, buf_b) return buf_a.id < buf_b.id end)<CR>
 
 " search
 nnoremap <leader><leader> :FZF<CR>
--- nvim/.config/nvim/lua/hbb/nvim-tree.lua
+++ nvim/.config/nvim/lua/hbb/nvim-tree.lua
@@ -1,50 +1,34 @@
--- setup with all defaults
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
-require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
-  auto_close = true,
-  auto_reload_on_write = true,
-  disable_netrw = false,
-  hide_root_folder = false,
-  hijack_cursor = false,
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+  return
+end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
+
+nvim_tree.setup {
+  disable_netrw = true,
   hijack_netrw = true,
-  hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = true,
-  open_on_tab = false,
-  sort_by = "name",
-  update_cwd = false,
-  view = {
-    width = 30,
-    height = 30,
-    side = "left",
-    preserve_window_proportions = false,
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes",
-    mappings = {
-      custom_only = false,
-      list = {
-        -- user mappings go here
-      },
-    },
+  open_on_setup = false,
+  ignore_ft_on_setup = {
+    "startify",
+    "dashboard",
+    "alpha",
   },
-  hijack_directories = {
+  auto_close = true,
+  open_on_tab = false,
+  hijack_cursor = false,
+  update_cwd = true,
+  update_to_buf_dir = {
     enable = true,
     auto_open = true,
   },
-  update_focused_file = {
-    enable = false,
-    update_cwd = false,
-    ignore_list = {},
-  },
-  ignore_ft_on_setup = {},
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
   diagnostics = {
-    enable = false,
-    show_on_dirs = false,
+    enable = true,
     icons = {
       hint = "",
       info = "",
@@ -52,45 +36,54 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       error = "",
     },
   },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
+  },
+  system_open = {
+    cmd = nil,
+    args = {},
+  },
   filters = {
     dotfiles = false,
     custom = {},
-    exclude = {},
   },
   git = {
     enable = true,
-    ignore = false,
-    timeout = 400,
+    ignore = true,
+    timeout = 500,
   },
-  actions = {
-    change_dir = {
-      enable = true,
-      global = false,
-    },
-    open_file = {
-      quit_on_open = false,
-      resize_window = false,
-      window_picker = {
-        enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = "left",
+    auto_resize = true,
+    mappings = {
+      custom_only = false,
+      list = {
+        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
+        { key = "v", cb = tree_cb "vsplit" },
       },
     },
+    number = false,
+    relativenumber = false,
   },
   trash = {
     cmd = "trash",
     require_confirm = true,
   },
-  log = {
-    enable = false,
-    truncate = false,
-    types = {
-      all = false,
-      config = false,
-      git = false,
-    },
+  quit_on_open = 0,
+  git_hl = 1,
+  disable_window_picker = 0,
+  root_folder_modifier = ":t",
+  show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 1,
+    tree_width = 30,
   },
-} -- END_DEFAULT_OPTS
+}
