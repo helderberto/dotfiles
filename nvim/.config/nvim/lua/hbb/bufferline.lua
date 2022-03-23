c8b1493eea378ff26d7b53e8acabddc5e935444f
Helder Burato Berto <helder.burato@gmail.com>
2022-03-23 19:51:10 +0000

    neovim: cleanup bufferline configurations

--- nvim/.config/nvim/lua/hbb/bufferline.lua
+++ nvim/.config/nvim/lua/hbb/bufferline.lua
@@ -6,19 +6,21 @@ end
 bufferline.setup {
   options = {
     numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-    close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+    -- close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+    -- right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+    -- left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
+    -- middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
     -- NOTE: this plugin is designed with this icon in mind,
     -- and so changing this is NOT recommended, this is intended
     -- as an escape hatch for people who cannot bear it for whatever reason
-    indicator_icon = "▎",
-    buffer_close_icon = "",
-    modified_icon = "●",
-    close_icon = "",
-    left_trunc_marker = "",
-    right_trunc_marker = "",
+    -- indicator_icon = "▎",
+    -- buffer_close_icon = "",
+    -- buffer_close_icon = '',
+    -- modified_icon = "●",
+    -- close_icon = "",
+    -- close_icon = '',
+    -- left_trunc_marker = "",
+    -- right_trunc_marker = "",
     --- name_formatter can be used to change the buffer's label in the bufferline.
     --- Please note some names can/will break the
     --- bufferline so use this at your discretion knowing that it has
