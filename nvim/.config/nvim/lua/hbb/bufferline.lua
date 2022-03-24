8b8ea9cdccb48feb2b94c8f688949e622c4e45dd
Helder Burato Berto <helder.burato@gmail.com>
2022-03-24 11:52:58 +0000

    neovim: bufferline only with more than one file

--- nvim/.config/nvim/lua/hbb/bufferline.lua
+++ nvim/.config/nvim/lua/hbb/bufferline.lua
@@ -65,7 +65,7 @@ bufferline.setup {
     -- [focused and unfocused]. eg: { '|', '|' }
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
     enforce_regular_tabs = true,
-    always_show_bufferline = true,
+    always_show_bufferline = false,
     -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
     --   -- add custom logic
     --   return buffer_a.modified > buffer_b.modified
