0d9019e8c71bf5c10f01368acae21685c51e6c22
Helder Burato Berto <helder.burato@gmail.com>
2022-03-31 18:32:34 +0100

    neovim: remove redundant telescope-fzf config

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -6,15 +6,6 @@ end
 local actions = require("telescope.actions")
 
 telescope.setup{
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    }
-  },
   defaults = {
     path_display = { "smart" },
     vimgrep_arguments = {
