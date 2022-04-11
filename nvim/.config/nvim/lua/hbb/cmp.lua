e7d520889bfcb17a26250212fcd1359aab26a953
Helder Burato Berto <helder.burato@gmail.com>
2022-04-11 19:07:49 +0100

    neovim: cmp load vscode snippets

--- nvim/.config/nvim/lua/hbb/cmp.lua
+++ nvim/.config/nvim/lua/hbb/cmp.lua
@@ -8,6 +8,8 @@ if not snip_status_ok then
   return
 end
 
+require('luasnip/loaders/from_vscode').lazy_load()
+
 local check_backspace = function()
   local col = vim.fn.col '.' - 1
   return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s'
