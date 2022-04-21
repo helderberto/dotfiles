2f625aa85c3fb8bb583b00eeb9a11b339342dc6f
Helder Burato Berto <helder.burato@gmail.com>
2022-04-21 19:11:38 +0100

    neovim: update null-ls diagnostics

--- nvim/.config/nvim/lua/hbb/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/hbb/lsp/null-ls.lua
@@ -15,23 +15,17 @@ local code_actions = null_ls.builtins.code_actions
 null_ls.setup {
   debug = false,
   sources = {
-    diagnostics.eslint.with { -- eslint or eslint_d
+    diagnostics.eslint.with {
       prefer_local = 'node_modules/.bin',
-    },
+    }, -- eslint or eslint_d
     code_actions.eslint, -- eslint or eslint_d
-    formatting.prettier,
+    formatting.prettier, -- prettier, eslint, eslint_d, or prettierd
     formatting.stylua,
     completion.spell,
   },
-  -- you can reuse a shared lspconfig on_attach callback here
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
-      vim.cmd [[
-        autocmd! * <buffer>
-          augroup LspFormatting
-          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-        augroup END
-      ]]
+      vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
     end
   end,
 }
