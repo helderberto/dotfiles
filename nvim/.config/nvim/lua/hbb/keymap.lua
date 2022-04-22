a66578d53096897e165005953f5419c89a53db59
Helder Burato Berto <helder.burato@gmail.com>
2022-04-22 18:29:22 +0100

    neovim: improve keymap utils

--- nvim/.config/nvim/lua/hbb/keymap.lua
+++ nvim/.config/nvim/lua/hbb/keymap.lua
@@ -8,19 +8,13 @@ local M = {}
 --  term_mode = "t"
 --  command_mode = "c"
 M.map = function(mode, shortcut, command, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, shortcut, command, options)
+  opts = opts or { noremap = true }
+  vim.api.nvim_set_keymap(mode, shortcut, command, opts)
 end
 
 M.buf_map = function(bufnr, mode, shortcut, command, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_buf_set_keymap(bufnr, mode, shortcut, command, options)
+  opts = opts or { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, mode, shortcut, command, opts)
 end
 
 return M
