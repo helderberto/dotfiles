09a1c3e0f2d15fd8fde6a3bde5c67e00ce9c3741
Helder Burato Berto <helder.burato@gmail.com>
2022-04-26 16:22:21 +0100

    neovim: check when lua client server

--- nvim/.config/nvim/lua/hbb/lsp/handlers.lua
+++ nvim/.config/nvim/lua/hbb/lsp/handlers.lua
@@ -87,7 +87,7 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == 'tsserver' or client.name == 'html' then
+  if client.name == 'tsserver' or client.name == 'html' or client.name == 'sumneko_lua' then
     client.resolved_capabilities.document_formatting = false
   end
 
