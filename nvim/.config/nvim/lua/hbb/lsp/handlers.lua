6fe1fe035f378c6300164212acf688f0a3d77f38
Helder Burato Berto <helder.burato@gmail.com>
2022-04-26 15:39:03 +0100

    neovim: format using null-ls specific clients

--- nvim/.config/nvim/lua/hbb/lsp/handlers.lua
+++ nvim/.config/nvim/lua/hbb/lsp/handlers.lua
@@ -87,8 +87,9 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  client.resolved_capabilities.document_formatting = false
-  client.resolved_capabilities.document_range_formatting = false
+  if client.name == 'tsserver' or client.name == 'html' then
+    client.resolved_capabilities.document_formatting = false
+  end
 
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
