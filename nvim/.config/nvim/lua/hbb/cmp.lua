3480d460a964b97baf6274ab91232ee61c91ff69
Helder Burato Berto <helder.burato@gmail.com>
2022-05-03 10:21:51 +0100

    neovim: add icons to cmp

--- nvim/.config/nvim/lua/hbb/cmp.lua
+++ nvim/.config/nvim/lua/hbb/cmp.lua
@@ -15,6 +15,36 @@ local check_backspace = function()
   return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s'
 end
 
+--   פּ ﯟ   some other good icons
+local kind_icons = {
+  Text = '',
+  Method = 'm',
+  Function = '',
+  Constructor = '',
+  Field = '',
+  Variable = '',
+  Class = '',
+  Interface = '',
+  Module = '',
+  Property = '',
+  Unit = '',
+  Value = '',
+  Enum = '',
+  Keyword = '',
+  Snippet = '',
+  Color = '',
+  File = '',
+  Reference = '',
+  Folder = '',
+  EnumMember = '',
+  Constant = '',
+  Struct = '',
+  Event = '',
+  Operator = '',
+  TypeParameter = '',
+}
+-- find more here: https://www.nerdfonts.com/cheat-sheet
+
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -55,7 +85,7 @@ cmp.setup {
   formatting = {
     fields = { 'kind', 'abbr', 'menu' },
     format = function(entry, vim_item)
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = '[LSP]',
         luasnip = '[Snippet]',
