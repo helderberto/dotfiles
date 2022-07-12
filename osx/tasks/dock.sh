9bc62930ff847d5b3e2058e1a70e8d97f824681d
Helder Burato Berto <helder.burato@gmail.com>
2022-07-12 19:24:27 +0100

    osx: add Notes to dock

--- osx/tasks/dock.sh
+++ osx/tasks/dock.sh
@@ -11,6 +11,7 @@ msg_install "Dock"
 add_app_to_dock "Google Chrome"
 add_app_to_dock "Alacritty"
 add_app_to_dock "Obsidian"
+add_app_to_dock "Notes"
 add_app_to_dock "Slack"
 add_app_to_dock "Spotify"
 add_folder_to_dock $HOME/Downloads -d 0 -s 2 -v 1
