709f14d6bf72e33bad6b91e12e7c2130e415e757
Helder Burato Berto <helder.burato@gmail.com>
2022-09-23 19:24:34 +0100

    nvim: load copilot configuration after load plugin

---
+++ nvim/.config/nvim/after/plugin/copilot.lua
@@ -0,0 +1,8 @@
+local map = require('hbb.keymap').map
+
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+
+map('i', '<C-e>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+map('i', '<C-k>', 'copilot#Previous()', { silent = true, expr = true })
+map('i', '<C-j>', 'copilot#Next()', { silent = true, expr = true })
