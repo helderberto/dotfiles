c7fb536f24be21cb579da54504934c721f307c8a
Helder Burato Berto <helder.burato@gmail.com>
2022-10-03 15:13:24 +0100

    nvim: update find git & vim options

--- nvim/.config/nvim/lua/hbb/telescope/mappings.lua
+++ nvim/.config/nvim/lua/hbb/telescope/mappings.lua
@@ -7,18 +7,18 @@ local map = require('hbb.keymap').map
 -- Files
 map('n', '<leader><leader>', ':Telescope find_files<cr>')
 map('n', '<leader>.', ':Telescope file_browser path=%:p:h<cr>')
-map('n', '<C-n>', ':Telescope file_browser<cr>')
+map('n', '<leader>ff', ':Telescope file_browser<cr>')
 map('n', '<leader>fg', ':Telescope live_grep<cr>')
 map('n', '<leader>fb', ':Telescope current_buffer_fuzzy_find<cr>')
 map('n', '<leader>fq', ':Telescope quickfix<cr>')
 
 -- Git
-map('n', '<leader>gs', ':Telescope git_status<cr>')
-map('n', '<leader>gl', ':Telescope git_commits<cr>')
+map('n', '<leader>fs', ':Telescope git_status<cr>')
+map('n', '<leader>fl', ':Telescope git_commits<cr>')
 
 -- Neovim
 map('n', '<leader>fh', ':Telescope help_tags<cr>')
-map('n', '<leader>bo', ':Telescope vim_options<cr>')
+map('n', '<leader>fo', ':Telescope vim_options<cr>')
 map('n', '<C-b>', ':Telescope buffers<cr>')
 
 -- Project
