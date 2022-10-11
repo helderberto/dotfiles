b7a3177d1a7727a72a8a48e68224da75c4adc4a4
Helder Burato Berto <helder.burato@gmail.com>
2022-10-11 19:15:49 +0100

    nvim: fix elixir lsp autocmd

--- nvim/.config/nvim/lua/hbb/lsp/handlers.lua
+++ nvim/.config/nvim/lua/hbb/lsp/handlers.lua
@@ -45,18 +45,13 @@ M.setup = function()
 end
 
 local function lsp_highlight_document(client)
-  -- Set autocommands conditional on server_capabilities
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
+  if client.server_capabilities.documentHighlightProvider then
+    vim.cmd [[
+      augroup LspFormatting
         autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
       augroup END
-    ]] ,
-      false
-    )
+    ]]
   end
 end
 
