6d0d6a5adea4681ee6d0f896b6e8bf1615406396
Helder Burato Berto <helder.burato@gmail.com>
2022-10-27 11:51:31 +0100

    nvim: update telescope mappings

--- nvim/.config/nvim/lua/hbb/telescope/mappings.lua
+++ nvim/.config/nvim/lua/hbb/telescope/mappings.lua
@@ -6,20 +6,18 @@ local map = require('hbb.keymap').map
 
 -- Files
 map('n', '<leader><leader>', ':Telescope find_files<cr>')
--- map('n', '<leader>.', ':Telescope file_browser path=%:p:h<cr>')
--- map('n', '<leader>ff', ':Telescope file_browser<cr>')
 map('n', '<leader>fg', ':Telescope live_grep<cr>')
-map('n', '<leader>fb', ':Telescope current_buffer_fuzzy_find<cr>')
+map('n', '<leader>ff', ':Telescope current_buffer_fuzzy_find<cr>')
 map('n', '<leader>fq', ':Telescope quickfix<cr>')
 
 -- Git
 map('n', '<leader>fs', ':Telescope git_status<cr>')
-map('n', '<leader>fl', ':Telescope git_commits<cr>')
+map('n', '<leader>fc', ':Telescope git_commits<cr>')
 
 -- Neovim
 map('n', '<leader>fh', ':Telescope help_tags<cr>')
 map('n', '<leader>fo', ':Telescope vim_options<cr>')
-map('n', '<C-b>', ':Telescope buffers<cr>')
+map('n', '<leader>fb', ':Telescope buffers<cr>')
 
 -- Project
 map('n', '<C-p>', ':Telescope project<CR>')
