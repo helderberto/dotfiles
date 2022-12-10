18ebdec5080cde245d9691347f657721ef5b68b1
Helder Burato Berto <helder.burato@gmail.com>
2022-12-10 16:42:18 +0000

    nvim: nvim-tree quit sidebar on open files

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -78,7 +78,7 @@ require('nvim-tree').setup {
       global = false,
     },
     open_file = {
-      quit_on_open = false,
+      quit_on_open = true,
       resize_window = true,
       window_picker = {
         enable = true,
