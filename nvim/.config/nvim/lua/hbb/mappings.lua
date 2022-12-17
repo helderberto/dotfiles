7bd7e2b8644b348650842f271fee5d32fbca8e4c
Helder Burato Berto <helder.burato@gmail.com>
2022-12-17 12:28:45 +0000

    nvim: remove mappings comments

--- nvim/.config/nvim/lua/hbb/mappings.lua
+++ nvim/.config/nvim/lua/hbb/mappings.lua
@@ -1,18 +1,14 @@
 local map = require('hbb.utils').map
 
--- Misc
 map('i', '<C-c>', '<esc>')
 map('n', '<esc>', ':noh<cr><cr>')
 
--- Saving
 map('n', '<leader>w', ':w!<cr>')
 map('n', '<leader>we', ':noa w<cr>')
 
--- Exiting
 map('n', '<leader>q', ':q<cr>')
 map('n', '<leader>qq', ':qa!<cr>')
 
--- Window explorer
 map('n', '<leader>e', '<C-w>')
 map('n', '<leader>ev', ':vsplit<cr>')
 map('n', '<leader>es', ':split<cr>')
@@ -20,26 +16,18 @@ map('n', '<leader>+', ':vertical resize +5<cr>')
 map('n', '<leader>-', ':vertical resize -5<cr>')
 map('n', '<leader>=', '<C-w>=<cr>')
 
--- Buffers
 map('n', '<leader>bd', ':bd<cr>')
 map('n', '<leader>bdd', ':bufdo bd<cr>')
 map('n', '<leader>;', '<C-^>') -- toggle last buffers
 
--- Better window navigation
 map('n', '<C-h>', '<C-w>h')
 map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-l>', '<C-w>l')
 
--- Select & movement
-
--- move line up
 map('v', 'J', ":m '>+1<CR>gv=gv")
 map('v', 'K', ":m '<-2<CR>gv=gv")
 map('v', 'p', '"_dP')
-
--- Source current configuration
-map('n', '<leader><cr>', ':so ~/.config/nvim/init.lua<cr>')
 
 -- Quickfix
 map('n', '<leader>co', ':call ToggleQuickFix()<CR>')
@@ -55,25 +43,20 @@ map('n', '<leader>cn', ':cnext<CR>')
 map('n', '<leader>cp', ':cprev<CR>')
 map('n', '<leader>cr', ':cfdo %s/')
 
--- Disable arrows
 map('n', '<Left>', ':lua print("Use h")<cr>')
 map('n', '<Right>', ':lua print("Use l")<cr>')
 map('n', '<Up>', ':lua print("Use k")<cr>')
 map('n', '<Down>', ':lua print("Use j")<cr>')
 
--- Git
 map('n', '<leader>g', ':G<cr>')
 map('n', '<leader>dh', ':diffget //2<cr>')
 map('n', '<leader>dl', ':diffget //3<cr>')
 
--- Search & Replace
 map('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
 
--- NvimTree
 map('n', '<leader>n', ':NvimTreeToggle<cr>')
 map('n', '<leader>.', ':NvimTreeFindFile<cr>')
 
--- Copilot
 map('i', '<C-h>', "copilot#Accept('<CR>')", { expr = true, replace_keycodes = false })
 map('i', '<C-k>', 'copilot#Previous()', { expr = true })
 map('i', '<C-j>', 'copilot#Next()', { expr = true })
