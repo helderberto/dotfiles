a65fb915cb2ae52eea5bb0b5c8d605af653a3b50
Helder Burato Berto <helder.burato@gmail.com>
2023-01-25 16:36:16 +0000

    nvim: disable netrw globally

--- nvim/.config/nvim/lua/hbb/settings.lua
+++ nvim/.config/nvim/lua/hbb/settings.lua
@@ -1,3 +1,5 @@
+vim.g.loaded_netrw = 1 -- Disable netrw
+vim.g.loaded_netrwPlugin = 1 -- Disable netrw
 vim.o.clipboard = 'unnamedplus' -- Use system clipboard
 vim.o.cursorline = true -- Find the current line quickly.
 vim.o.scrolloff = 8 -- Scroll screen after 8 lines
