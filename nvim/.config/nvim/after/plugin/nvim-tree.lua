6f6f2e25f01c9b500b142a819e58d9e0dc52382f
Helder Burato Berto <helder.burato@gmail.com>
2023-01-30 10:36:06 +0000

    nvim: nvim-tree open on startup

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -1,3 +1,10 @@
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
+local function open_nvim_tree()
+  require('nvim-tree.api').tree.open()
+end
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
+
 require('nvim-tree').setup({
   view = {
     width = 40,
