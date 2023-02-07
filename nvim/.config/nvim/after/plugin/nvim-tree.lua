30aa6e60f7652d8406bbcbcdb395c1b6bcd93eff
Helder Burato Berto <helder.burato@gmail.com>
2023-02-07 10:28:47 +0000

    nvim: update nvim-tree config

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -1,6 +1,8 @@
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
+
 local function open_nvim_tree()
-  require('nvim-tree.api').tree.open()
+  -- open the tree but don't focus it
+  require('nvim-tree.api').tree.toggle({ focus = false })
 end
 
 vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
