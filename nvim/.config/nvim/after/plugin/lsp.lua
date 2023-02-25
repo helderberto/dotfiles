4e50925d825d2ec3874c7c01c13eff6246b23441
Helder Burato Berto <helder.burato@gmail.com>
2023-02-25 13:04:12 +0000

    nvim: add desc to lsp keymaps

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -51,21 +51,78 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(_, bufnr)
-  local opts = { buffer = bufnr, remap = false }
-
-  map('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>', opts)
-  map('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', opts)
-  map('n', 'gI', ':lua vim.lsp.buf.implementation()<CR>', opts)
-  map('n', 'gr', ':lua vim.lsp.buf.references()<CR>', opts)
-  map('n', 'K', ':lua vim.lsp.buf.hover()<CR>', opts)
-  map('n', '<leader>lf', ':lua vim.lsp.buf.format { async = true }<CR>', opts)
-  map('n', '<leader>lr', ':lua vim.lsp.buf.rename()<CR>', opts)
-  map('n', '<leader>lk', ':lua vim.diagnostic.goto_prev()<CR>', opts)
-  map('n', '<leader>lj', ':lua vim.diagnostic.goto_next()<CR>', opts)
-  map('n', '<leader>la', ':lua vim.lsp.buf.code_action()<CR>', opts)
-  map('n', '<leader>ls', ':lua vim.lsp.buf.signature_help()<CR>', opts)
-  map('n', '<leader>li', ':LspInfo<CR>', opts)
-  map('n', '<leader>lI', ':LspInstallInfo<CR>', opts)
+  map('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Go to declaration',
+  })
+  map('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Go to definition',
+  })
+  map(
+    'n',
+    'gI',
+    ':lua vim.lsp.buf.implementation()<CR>',
+    { buffer = bufnr, noremap = true, desc = 'Go to implementation' }
+  )
+  map('n', 'gr', ':lua vim.lsp.buf.references()<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Go to references',
+  })
+  map('n', 'gt', ':lua vim.lsp.buf.type_definition', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Go to type definition',
+  })
+  map('n', 'K', ':lua vim.lsp.buf.hover()<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Hover',
+  })
+  map('n', '<leader>lf', ':lua vim.lsp.buf.format { async = true }<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Format',
+  })
+  map('n', '<leader>lr', ':lua vim.lsp.buf.rename()<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Rename',
+  })
+  map('n', '<leader>lk', ':lua vim.diagnostic.goto_prev()<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Go to previous diagnostic',
+  })
+  map('n', '<leader>lj', ':lua vim.diagnostic.goto_next()<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Go to next diagnostic',
+  })
+  map(
+    { 'n', 'v' },
+    '<leader>la',
+    ':lua vim.lsp.buf.code_action()<CR>',
+    { buffer = bufnr, noremap = true, desc = 'Code Action' }
+  )
+  map('n', '<leader>ls', ':lua vim.lsp.buf.signature_help()<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'Signature Help',
+  })
+  map('n', '<leader>li', ':LspInfo<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'LSP Info',
+  })
+  map('n', '<leader>lI', ':LspInstallInfo<CR>', {
+    buffer = bufnr,
+    noremap = true,
+    desc = 'LSP Install Info',
+  })
 end)
 
 local null_ls = require('null-ls')
