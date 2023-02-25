8897e912a7efb9e5107af877e0bef03be25b5453
Helder Burato Berto <helder.burato@gmail.com>
2023-02-25 12:38:43 +0000

    nvim: update default settings
    Resources:
    - https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
    - https://github.com/LunarVim/LunarVim/blob/master/lua/lvim/config/settings.lua

--- nvim/.config/nvim/lua/hbb/settings.lua
+++ nvim/.config/nvim/lua/hbb/settings.lua
@@ -3,40 +3,37 @@ vim.o.cursorline = true -- Find the current line quickly.
 vim.o.scrolloff = 8 -- Scroll screen after 8 lines
 vim.o.mouse = 'a' -- Enable mouse support
 vim.o.colorcolumn = '120' -- Highlight column 120
-vim.o.number = true -- show line numbers
+vim.o.number = true -- show line number
 vim.o.relativenumber = true -- show line number relative to the current line
-vim.o.ignorecase = true -- ignore letter case when searching
 vim.o.smartcase = true -- case insentive unless capitals used in search
-vim.o.hlsearch = false -- Set highlight on search
+vim.o.smartindent = true -- make indenting smarter again
+vim.o.hlsearch = true -- highlight all matches on previous search pattern
+vim.o.ignorecase = true -- ignore case in search patterns
 vim.o.title = true -- Show title at top of the terminal
-vim.o.timeoutlen = 300 -- VIM hold up 300ms after key press
+vim.o.titlestring = '%<%F%=%l/%L - nvim' -- what the title of the window will be set to
+vim.o.timeoutlen = 300 -- hold up 300ms after key press
 vim.o.hidden = true -- TextEdit might fail if hidden is not set
 vim.o.signcolumn = 'yes'
 vim.o.updatetime = 50 -- Update delay to 5ms
-
--- Split natural directions
-vim.o.splitbelow = true
-vim.o.splitright = true
-
--- Folding
-vim.o.foldmethod = 'expr'
-vim.o.foldlevelstart = 99 -- Start file with all folds opened
-vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-
--- Indentation
-vim.bo.expandtab = true -- turn tabs into tabstop spaces
-vim.bo.tabstop = 2 -- 1 tab = 2 spaces
-vim.bo.shiftwidth = 2 -- shift 2 spaces
-
-vim.o.list = true
-vim.o.listchars = 'eol:¬,space:⋅' -- show empty characters
-vim.o.backup = false -- don"t use backup files
+vim.o.splitbelow = true -- Horizontal splits will automatically be below
+vim.o.splitright = true -- Vertical splits will automatically be to the right
+vim.o.foldmethod = 'expr' -- Folding method (expr, syntax, indent)
+vim.o.foldlevelstart = 99 -- Start folded (0 = all unfolded, 99 = all folded)
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()' -- Fold based on treesitter syntax
+vim.bo.expandtab = true -- Use spaces instead of tabs
+vim.bo.tabstop = 2 -- Insert 2 spaces for a tab
+vim.bo.shiftwidth = 2 -- Change the number of space characters inserted for indentation
+vim.o.list = true -- Show some invisible characters (tabs...)
+vim.o.listchars = 'eol:¬,space:⋅' -- Unicode characters used for whitespace
+vim.o.backup = false -- don"t create backup file
 vim.o.writebackup = false
 vim.o.swapfile = false -- don"t use swap file
-vim.o.termguicolors = true -- force GUI colors in terminals
-vim.cmd('colorscheme dracula') -- Set colorscheme
+vim.o.termguicolors = true -- set term gui colors most terminals support this
+vim.o.showmode = false -- Dont show mode since we have a statusline
+vim.o.updatetime = 250 -- Faster completion (default is 4000 ms = 4 s)
 
--- You will likely want to reduce updatetime which affects CursorHold
--- note: this setting is global and should be set only once
-vim.o.updatetime = 250
+-- Enable theme Dracula
+vim.cmd('colorscheme dracula')
+
+-- Show diagnostics on cursor hold (default 500ms)
 vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
