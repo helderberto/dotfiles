83898f012f08686c7a345a7328ba6af034c222d4
Helder Burato Berto <helder.burato@gmail.com>
2023-05-10 16:24:31 +0100

    nvim: add window keymaps

--- nvim/.config/nvim/lua/hbb/keymaps.lua
+++ nvim/.config/nvim/lua/hbb/keymaps.lua
@@ -38,6 +38,9 @@ map('n', '<C-h>', '<C-w>h', { desc = 'Go to left window' })
 map('n', '<C-j>', '<C-w>j', { desc = 'Go to lower window' })
 map('n', '<C-k>', '<C-w>k', { desc = 'Go to upper window' })
 map('n', '<C-l>', '<C-w>l', { desc = 'Go to right window' })
+map('n', '<leader>e', '<C-w>', { desc = 'Window' })
+map('n', '<leader>es', '<C-w>s', { desc = 'Split window' })
+map('n', '<leader>ev', '<C-w>v', { desc = 'Split window vertically' })
 
 -- better up/down
 map('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line up' })
