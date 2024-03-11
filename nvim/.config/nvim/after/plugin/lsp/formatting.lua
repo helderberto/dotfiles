c3f68853ad5f282a0a38a02f1a68816a5cfc3aec
Helder Burato Berto <helder.burato@gmail.com>
2024-03-11 22:52:12 +0000

    nvim: fix formatting

--- nvim/.config/nvim/after/plugin/lsp/formatting.lua
+++ nvim/.config/nvim/after/plugin/lsp/formatting.lua
@@ -12,16 +12,45 @@ conform.setup({
     html = { 'htmlbeautifier' },
   },
 
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  -- format_on_save = function(bufnr)
+  --   -- Disable with a global or buffer-local variable
+  --   if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+  --     return
+  --   end
+  --   return { timeout_ms = 500, lsp_fallback = true }
+  -- end,
 })
 
-vim.keymap.set({ 'n', 'v' }, '<leader>F', function()
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
+  callback = function(args)
+    require('conform').format({ bufnr = args.buf })
+  end,
+})
+
+vim.keymap.set({ 'n', 'v' }, '<leader>ll', function()
   conform.format({
     lsp_fallback = true,
     async = false,
     timeout_ms = 1000,
   })
 end, { desc = 'Format file or range (in visual mode)' })
+--
+-- vim.api.nvim_create_user_command('FormatDisable', function(args)
+--   if args.bang then
+--     -- FormatDisable! will disable formatting just for this buffer
+--     vim.b.disable_autoformat = true
+--   else
+--     vim.g.disable_autoformat = true
+--   end
+-- end, {
+--   desc = 'Disable autoformat-on-save',
+--   bang = true,
+-- })
+--
+-- vim.api.nvim_create_user_command('FormatEnable', function()
+--   vim.b.disable_autoformat = false
+--   vim.g.disable_autoformat = false
+-- end, {
+--   desc = 'Re-enable autoformat-on-save',
+-- })
