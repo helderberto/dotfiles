f98908c320359fb6914cdad223e2e47139face9e
Helder Burato Berto <helder.burato@gmail.com>
2024-03-11 22:52:21 +0000

    nvim: remove lint keymap

--- nvim/.config/nvim/after/plugin/lsp/linting.lua
+++ nvim/.config/nvim/after/plugin/lsp/linting.lua
@@ -1,17 +1,11 @@
 local lint = require('lint')
 
-local map = require('hbb.utils').map
-
 lint.linters_by_ft = {
   javascript = { 'eslint_d' },
   typescript = { 'eslint_d' },
   javascriptreact = { 'eslint_d' },
   typescriptreact = { 'eslint_d' },
 }
-
-map('n', '<leader>ll', function()
-  lint.try_lint()
-end, { desc = 'Trigger linting for current file' })
 
 local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 
