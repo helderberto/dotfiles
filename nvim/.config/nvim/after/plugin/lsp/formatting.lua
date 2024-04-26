a2d68c57ab5b9c79d8b8f7e1ade17369741192c8
Helder Burato Berto <helder.burato@gmail.com>
2024-04-26 15:26:49 +0100

    nvim: add enable/disable formatting command

--- nvim/.config/nvim/after/plugin/lsp/formatting.lua
+++ nvim/.config/nvim/after/plugin/lsp/formatting.lua
@@ -13,20 +13,37 @@ conform.setup({
   },
 
   format_on_save = function(bufnr)
-    -- Disable autoformat on certain filetypes
-    local ignore_filetypes = { 'sql', 'java' }
-    if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-      return
-    end
     -- Disable with a global or buffer-local variable
     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
       return
     end
+
     -- Disable autoformat for files in a certain path
     local bufname = vim.api.nvim_buf_get_name(bufnr)
     if bufname:match('/node_modules/') then
       return
     end
+
     return { timeout_ms = 500, lsp_fallback = true }
   end,
+})
+
+-- Commands to enable/disable autoformat-on-save
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
 })
