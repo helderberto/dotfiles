c8001d7534d65c552c571934fea0753ec03d365d
Helder Burato Berto <helder.burato@gmail.com>
2024-05-03 16:23:28 +0100

    nvim: lint using eslint instead of eslint_d

--- nvim/.config/nvim/after/plugin/lsp/linting.lua
+++ nvim/.config/nvim/after/plugin/lsp/linting.lua
@@ -1,10 +1,10 @@
 local lint = require('lint')
 
 lint.linters_by_ft = {
-  javascript = { 'eslint_d' },
-  typescript = { 'eslint_d' },
-  javascriptreact = { 'eslint_d' },
-  typescriptreact = { 'eslint_d' },
+  javascript = { 'eslint' },
+  typescript = { 'eslint' },
+  javascriptreact = { 'eslint' },
+  typescriptreact = { 'eslint' },
 }
 
 local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
