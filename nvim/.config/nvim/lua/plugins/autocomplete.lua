ded4be70d1d1845ccfcdd532749ce6a1f217a6bd
Helder Burato Berto <helder.burato@gmail.com>
2024-05-03 19:14:55 +0100

    nvim: cmp plugin config

---
+++ nvim/.config/nvim/lua/plugins/autocomplete.lua
@@ -0,0 +1,105 @@
+return {
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
+  { 'hrsh7th/cmp-nvim-lua' },
+
+  -- Snippets
+  {
+    'L3MON4D3/LuaSnip',
+    dependencies = {
+      'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets',
+    },
+  },
+
+  {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp_status_ok, cmp = pcall(require, 'cmp')
+      if not cmp_status_ok then
+        return
+      end
+
+      require('luasnip/loaders/from_vscode').lazy_load()
+
+      local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+      local kind_icons = {
+        Text = '󰀬',
+        Method = '󰠄',
+        Function = '󰊕',
+        Constructor = '',
+        Field = '',
+        Variable = '󰫧',
+        Class = '',
+        Interface = '',
+        Module = '',
+        Property = '',
+        Unit = '',
+        Value = '󰗧',
+        Enum = '',
+        Keyword = '',
+        Snippet = '',
+        Color = '',
+        File = '',
+        Reference = '',
+        Folder = '',
+        EnumMember = '',
+        Constant = '',
+        Struct = '',
+        Event = '',
+        Operator = '',
+        TypeParameter = '󰊄',
+      }
+      -- find more here: https://www.nerdfonts.com/cheat-sheet
+
+      -- Disable <Tab> and <S-Tab> mappings
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          -- Accept currently selected item. If none selected, `select` first item.
+          -- Set `select` to `false` to only confirm explicitly selected items.
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = nil,
+          ['<S-Tab>'] = nil,
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer', keyword_length = 4 },
+          { name = 'path' },
+          { name = 'nvim_lua' },
+        },
+        formatting = {
+          fields = { 'kind', 'abbr', 'menu' },
+          format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
+            -- Source
+            vim_item.menu = ({
+              nvim_lsp = '[LSP]',
+              luasnip = '[Snippet]',
+              buffer = '[Buffer]',
+              path = '[Path]',
+              nvim_lua = '[Lua]',
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+        confirm_opts = {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        },
+      })
+    end,
+  },
+}
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,18 +5,6 @@ return {
     { 'neovim/nvim-lspconfig' },
     { 'williamboman/mason.nvim' },
     { 'williamboman/mason-lspconfig.nvim' },
-
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-nvim-lua' },
-
-    -- Snippets
-    { 'L3MON4D3/LuaSnip' },
-    { 'rafamadriz/friendly-snippets' },
   },
 
   config = function()
@@ -145,90 +133,5 @@ return {
 
     -- Mason
     vim.keymap.set('n', '<leader>lI', ':Mason<cr>', { desc = 'Mason' })
-
-    local cmp_status_ok, cmp = pcall(require, 'cmp')
-    if not cmp_status_ok then
-      return
-    end
-
-    require('luasnip/loaders/from_vscode').lazy_load()
-
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-    local kind_icons = {
-      Text = '󰀬',
-      Method = '󰠄',
-      Function = '󰊕',
-      Constructor = '',
-      Field = '',
-      Variable = '󰫧',
-      Class = '',
-      Interface = '',
-      Module = '',
-      Property = '',
-      Unit = '',
-      Value = '󰗧',
-      Enum = '',
-      Keyword = '',
-      Snippet = '',
-      Color = '',
-      File = '',
-      Reference = '',
-      Folder = '',
-      EnumMember = '',
-      Constant = '',
-      Struct = '',
-      Event = '',
-      Operator = '',
-      TypeParameter = '󰊄',
-    }
-    -- find more here: https://www.nerdfonts.com/cheat-sheet
-
-    -- Disable <Tab> and <S-Tab> mappings
-
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-      },
-      mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        -- Accept currently selected item. If none selected, `select` first item.
-        -- Set `select` to `false` to only confirm explicitly selected items.
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = nil,
-        ['<S-Tab>'] = nil,
-      },
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'buffer', keyword_length = 4 },
-        { name = 'path' },
-        { name = 'nvim_lua' },
-      },
-      formatting = {
-        fields = { 'kind', 'abbr', 'menu' },
-        format = function(entry, vim_item)
-          -- Kind icons
-          vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
-          -- Source
-          vim_item.menu = ({
-            nvim_lsp = '[LSP]',
-            luasnip = '[Snippet]',
-            buffer = '[Buffer]',
-            path = '[Path]',
-            nvim_lua = '[Lua]',
-          })[entry.source.name]
-          return vim_item
-        end,
-      },
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      },
-    })
   end,
 }
