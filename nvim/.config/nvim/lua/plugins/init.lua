8dc4097ec4b3a0e4e2b06788a3ac98ec8a4a1ff9
Helder Burato Berto <helder.burato@gmail.com>
2024-05-03 18:21:52 +0100

    nvim: lualine plugin config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,6 +1,5 @@
 return {
   'tpope/vim-repeat', -- Enable repeating supported plugin maps with "."
-  'nvim-lualine/lualine.nvim', -- Status line
   'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
   {
     'RRethy/vim-illuminate', -- Highlight all instances of the word under the cursor
---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,32 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  config = function()
+    require('lualine').setup({
+      options = {
+        icons_enabled = true,
+        theme = 'dracula',
+        component_separators = '',
+        section_separators = '',
+        disabled_filetypes = {},
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = {},
+    })
+  end,
+}
