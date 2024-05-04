f8ed24250a5b8bbd3187d6ee77d8d5cd2ea6bdbc
Helder Burato Berto <helder.burato@gmail.com>
2024-05-04 14:42:41 +0100

    osx(dock): re-organize

--- osx/tasks/dock.sh
+++ osx/tasks/dock.sh
@@ -8,9 +8,10 @@ clear_dock
 disable_recent_apps_from_dock
 
 msg_install "Dock"
-add_app_to_dock "Google Chrome"
+add_app_to_dock "Arc"
+add_app_to_dock "Obsidian"
+add_app_to_dock "Visual Studio Code"
 add_app_to_dock "Alacritty"
-add_app_to_dock "Logseq"
 add_app_to_dock "Slack"
 add_app_to_dock "Spotify"
 add_folder_to_dock $HOME/Downloads -d 0 -s 2 -v 1
