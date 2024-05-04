c24804a11a72cc9681849988623c5b8d08ed1f36
Helder Burato Berto <helder.burato@gmail.com>
2024-05-04 10:55:48 +0100

    nvim: refactor Treesitter config

--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'windwp/nvim-autopairs', -- Insert or delete brackets, parens, quotes in pair
-
-  config = function()
-    require('nvim-autopairs').setup({
-      disable_filetype = { 'TelescopePrompt', 'vim' },
-    })
-  end,
-}
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,18 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = function()
-    require('nvim-treesitter.install').update({ with_sync = true })
-  end,
-  config = function()
-    require('nvim-treesitter.configs').setup({
+  -- Treesitter is a parser generator tool and an incremental parsing library. It can build a concrete syntax tree for a source file and efficiently update the syntax tree as the source file is edited.
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
+    init = function()
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require('nvim-treesitter.query_predicates')
+    end,
+    opts = {
       ensure_installed = {
         'bash',
         'css',
@@ -39,9 +47,32 @@ return {
       languages = {
         typescript = '// %s',
       },
-    })
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
 
-    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-    parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
-  end,
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
+    end,
+  },
+
+  -- Automatically add closing tags for HTML and JSX
+  {
+    'windwp/nvim-ts-autotag',
+
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
+
+  -- Insert or delete brackets, parens, quotes in pair
+  {
+    'windwp/nvim-autopairs',
+
+    config = function()
+      require('nvim-autopairs').setup({
+        disable_filetype = { 'TelescopePrompt', 'vim' },
+      })
+    end,
+  },
 }
