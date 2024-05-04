7a9e0b138cb28f455bc1b414afe2f6d4116a5f77
Helder Burato Berto <helder.burato@gmail.com>
2024-05-04 10:13:55 +0100

    nvim: clean up illuminate

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,10 +1,4 @@
 return {
   'tpope/vim-repeat', -- Enable repeating supported plugin maps with "."
-  {
-    'RRethy/vim-illuminate', -- Highlight all instances of the word under the cursor
-    config = function()
-      vim.g.Illuminate_delay = 100
-    end,
-  },
   'github/copilot.vim',
 }
