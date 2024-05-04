fb93d890da5907ba4083b205a47988808a2cc55b
Helder Burato Berto <helder.burato@gmail.com>
2024-05-04 11:02:36 +0100

    nvim: git update keymaps

--- nvim/.config/nvim/lua/plugins/git-stuff.lua
+++ nvim/.config/nvim/lua/plugins/git-stuff.lua
@@ -5,11 +5,7 @@ return {
       vim.keymap.set('n', '<leader>gg', ':G<cr>', { desc = 'Git status' })
       vim.keymap.set('n', '<leader>dh', ':diffget //2<cr>', { desc = 'Git Diff Hunk' })
       vim.keymap.set('n', '<leader>dl', ':diffget //3<cr>', { desc = 'Git Diff Left' })
-      vim.keymap.set('n', '<leader>ga', '<cmd>G aa<cr>', { desc = 'Git stage all' })
-      vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = 'Git commit' })
-      vim.keymap.set('n', '<leader>gca', '<cmd>Git ca<cr>', { desc = 'Git commit amend' })
       vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<cr>', { desc = 'Git diff' })
-      vim.keymap.set('n', '<leader>gl', '<cmd>Git log --graph --oneline --decorate --all<cr>', { desc = 'Git log' })
       vim.keymap.set('n', '<leader>gu', '<cmd>Git pull<cr>', { desc = 'Git pull' })
       vim.keymap.set('n', '<leader>gp', '<cmd>Git ps<cr>', { desc = 'Git push' })
       vim.keymap.set('n', '<leader>gP', '<cmd>Git pf<cr>', { desc = 'Git push force' })
@@ -24,6 +20,7 @@ return {
 
       -- Keymaps
       vim.keymap.set('n', '<leader>gt', ':Gitsigns blame_line<cr>', { desc = 'Git Blame' })
+      vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<cr>', { desc = 'Git Preview Hunk' })
     end,
   },
 }
