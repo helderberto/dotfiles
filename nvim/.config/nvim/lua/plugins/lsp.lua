79a69fb70df7ff8aa035181465500f38547452dc
Helder Burato Berto <helder.burato@gmail.com>
2024-09-13 16:15:44 +0100

    nvim: fix ts server

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -41,7 +41,7 @@ return {
     require('mason').setup({})
     require('mason-lspconfig').setup({
       ensure_installed = {
-        'tsserver',
+        'ts_ls',
         'eslint',
         'cssls',
         'html',
