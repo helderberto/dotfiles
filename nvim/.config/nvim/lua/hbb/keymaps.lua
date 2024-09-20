cd7e6372e2ee34368ab834221c3f2a7f6b7798a4
Helder Burato Berto <helder.burato@gmail.com>
2024-09-20 21:03:35 +0100

    nvim; update general maps

--- nvim/.config/nvim/lua/hbb/keymaps.lua
+++ nvim/.config/nvim/lua/hbb/keymaps.lua
@@ -1,57 +1,58 @@
-local set = vim.keymap.set
-
 --- save file
-set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
-set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
-set('n', '<leader>ww', '', { desc = 'Save without format' })
+vim.keymap.set('n', '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
+vim.keymap.set('n', '<leader>s', '<cmd>w<cr>', { desc = 'Save file' })
+vim.keymap.set('n', '<leader>S', '<cmd>noautocmd w<cr>', { desc = 'Save without format' })
 
 -- quit
-set('n', '<leader>q', '<cmd>q<cr>', { desc = 'Quit buffer' })
-set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All' })
+vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', { desc = 'Quit buffer' })
+vim.keymap.set('n', '<leader>Q', '<cmd>qa<cr>', { desc = 'Quit All' })
 
 -- better indenting
-set('v', '<', '<gv')
-set('v', '>', '>gv')
+vim.keymap.set('v', '<', '<gv', { desc = 'Indent left' })
+vim.keymap.set('v', '>', '>gv', { desc = 'Indent right' })
 
-set('n', '<leader>x', '<cmd>.lua<cr>', { desc = 'Execute the current line' })
-set('n', '<leader><leader>x', '<cmd>source %<cr>', { desc = 'Execute current file' })
+-- execute code
+vim.keymap.set('n', '<leader>x', '<cmd>.lua<cr>', { desc = 'Execute the current line' })
+vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<cr>', { desc = 'Execute current file' })
 
 -- clear search with <esc>
-set({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and Clear hlsearch' })
+vim.keymap.set({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and Clear hlsearch' })
 
 -- new file
-set('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
+vim.keymap.set('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
 
 -- buffers
-set('n', '<S-l>', ':bnext<CR>', { desc = 'Next buffer' })
-set('n', '<S-h>', ':bprevious<CR>', { desc = 'Previous buffer' })
-set('n', '<leader>bd', ':bdelete<cr>', { desc = 'Buffer Delete' })
-set('n', '<leader>bD', ':bufdo bd<cr>', { desc = 'Buffer Delete all' })
-set('n', '<leader>;', '<C-^>', { desc = 'Toggle last buffers' }) -- toggle last buffers
+vim.keymap.set('n', '<S-l>', ':bnext<CR>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<S-h>', ':bprevious<CR>', { desc = 'Previous buffer' })
+vim.keymap.set('n', '<leader>bd', ':bdelete<cr>', { desc = 'Buffer Delete' })
+vim.keymap.set('n', '<leader>bD', ':bufdo bd<cr>', { desc = 'Buffer Delete all' })
+vim.keymap.set('n', '<leader>;', '<C-^>', { desc = 'Toggle last buffers' }) -- toggle last buffers
 
 -- better window navigation
-set('n', '<leader>e', '<C-w>', { desc = 'Window' })
-set('n', '<leader>es', '<C-w>s', { desc = 'Split window' })
-set('n', '<leader>ev', '<C-w>v', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>w', '<C-w>', { desc = 'Window' })
+vim.keymap.set('n', '<leader>ws', '<C-w>s', { desc = 'Horizontal split' })
+vim.keymap.set('n', '<leader>wv', '<C-w>v', { desc = 'Vertical split' })
+vim.keymap.set('n', '<leader>wq', '<C-w>q', { desc = 'Close window' })
+vim.keymap.set('n', '<leader>wo', '<C-w>o', { desc = 'Close other windows' })
 
 -- better up/down
-set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line up' })
-set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line down' })
-set('v', 'p', '"_dP', { desc = '[P] paste' })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line up' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line down' })
+vim.keymap.set('v', 'p', '"_dP', { desc = '[P] paste' })
 
 -- quickfix
-set('n', '<leader>ck', ':cexpr []<cr>', { desc = 'Clear list' })
-set('n', '<leader>cc', ':cclose <cr>', { desc = 'Close list' })
-set('n', '<leader>co', ':copen <cr>', { desc = 'Open list' })
-set('n', '<leader>cf', ':cfdo %s/', { desc = 'Search & Replace' })
-set('n', '<leader>cp', ':cprev<cr>zz', { desc = 'Prev Item' })
-set('n', '<leader>cn', ':cnext<cr>zz', { desc = 'Next Item' })
+vim.keymap.set('n', '<leader>ck', ':cexpr []<cr>', { desc = 'Clear list' })
+vim.keymap.set('n', '<leader>cc', ':cclose <cr>', { desc = 'Close list' })
+vim.keymap.set('n', '<leader>co', ':copen <cr>', { desc = 'Open list' })
+vim.keymap.set('n', '<leader>cf', ':cfdo %s/', { desc = 'Search & Replace' })
+vim.keymap.set('n', '<leader>cp', ':cprev<cr>zz', { desc = 'Prev Item' })
+vim.keymap.set('n', '<leader>cn', ':cnext<cr>zz', { desc = 'Next Item' })
 
 -- search & replace
-set('n', '<leader>rr', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Replace word' })
+vim.keymap.set('n', '<leader>sr', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Replace word' })
 
 -- toggle relative number
-set('n', '<leader>rn', ':set relativenumber!<cr>', { desc = 'Relative Number' })
+vim.keymap.set('n', '<leader>lr', ':set relativenumber!<cr>', { desc = 'Toggle relative number' })
 
 -- lazy
-set('n', '<leader>ll', '<cmd>Lazy<cr>', { desc = 'Lazy' })
+vim.keymap.set('n', '<leader>ll', '<cmd>Lazy<cr>', { desc = 'Lazy' })
