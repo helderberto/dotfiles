57aff2187273cdc52a7c0bac18502fc04a18c4b2
Helder Burato Berto <helder.burato@gmail.com>
2024-09-30 20:15:46 +0100

    nvim: update git keymaps

--- nvim/.config/nvim/lua/plugins/git-stuff.lua
+++ nvim/.config/nvim/lua/plugins/git-stuff.lua
@@ -2,13 +2,29 @@ return {
   {
     'tpope/vim-fugitive', -- Git commands in nvim
     config = function()
+      -- Git status (Neogit-like binding)
       vim.keymap.set('n', '<leader>gg', ':G<cr>', { desc = 'Git status' })
-      vim.keymap.set('n', '<leader>dh', ':diffget //2<cr>', { desc = 'Git Diff Hunk' })
-      vim.keymap.set('n', '<leader>dl', ':diffget //3<cr>', { desc = 'Git Diff Left' })
-      vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<cr>', { desc = 'Git diff' })
+
+      -- Git diff and merge helpers
+      vim.keymap.set('n', '<leader>gh', ':diffget //2<cr>', { desc = 'Git Diff Hunk (ours)' }) -- Current change
+      vim.keymap.set('n', '<leader>gl', ':diffget //3<cr>', { desc = 'Git Diff Hunk (theirs)' }) -- Incoming change
+      vim.keymap.set('n', '<leader>gd', '<cmd>Gvdiffsplit<cr>', { desc = 'Git diff (vertical split)' })
+
+      -- Git pull/push commands
       vim.keymap.set('n', '<leader>gu', '<cmd>Git pull<cr>', { desc = 'Git pull' })
-      vim.keymap.set('n', '<leader>gp', '<cmd>Git ps<cr>', { desc = 'Git push' })
-      vim.keymap.set('n', '<leader>gP', '<cmd>Git pf<cr>', { desc = 'Git push force' })
+      vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { desc = 'Git push' })
+      vim.keymap.set('n', '<leader>gP', '<cmd>Git push --force<cr>', { desc = 'Git push force' })
+
+      -- Git commit and stage
+      vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = 'Git commit' })
+      vim.keymap.set('n', '<leader>ga', '<cmd>Git add .<cr>', { desc = 'Git add all' })
+
+      -- Git staging shortcuts (Neogit-like experience)
+      vim.keymap.set('n', '<leader>gs', '<cmd>Gwrite<cr>', { desc = 'Git stage' })
+      vim.keymap.set('n', '<leader>gr', '<cmd>Gread<cr>', { desc = 'Git reset' }) -- Unstage/reset
+
+      -- Browse git log
+      vim.keymap.set('n', '<leader>glg', '<cmd>Git log<cr>', { desc = 'Git log' })
     end,
   },
 
