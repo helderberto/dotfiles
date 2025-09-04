402e95484223a110f30d97809f14068b9cf09ef7
Helder Burato Berto <helder.burato@gmail.com>
2025-09-04 12:50:56 +0100

    nvim: update toggleterm

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -3,26 +3,75 @@ return {
   version = '*',
   config = function()
     require('toggleterm').setup({
-      size = 20, -- Terminal size (height for horizontal, width for vertical)
-      open_mapping = [[<C-\>]], -- Keybinding to toggle the terminal
-      hide_numbers = true, -- Hide line numbers in terminal buffers
-      shade_terminals = true, -- Apply shading to terminal backgrounds
-      shading_factor = 2, -- Degree of terminal background shading
-      start_in_insert = true, -- Start terminal in insert mode
-      insert_mappings = true, -- Enable keybinding in insert mode
-      terminal_mappings = true, -- Enable keybinding in terminal mode
-      persist_size = true, -- Remember terminal size between sessions
-      direction = 'horizontal', -- Terminal direction: 'vertical', 'horizontal', 'tab', or 'float'
-      close_on_exit = true, -- Close terminal when the process exits
-      shell = vim.o.shell, -- Use the default shell configured in Neovim
+      -- Dynamic sizing based on direction
+      size = function(term)
+        if term.direction == 'horizontal' then
+          return 15
+        elseif term.direction == 'vertical' then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<C-\>]], -- Main toggle keybinding
+      hide_numbers = true,
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      persist_size = true,
+      direction = 'horizontal', -- Default direction
+      close_on_exit = true,
+      shell = vim.o.shell,
+      -- Float terminal configuration
+      float_opts = {
+        border = 'curved',
+        width = math.floor(vim.o.columns * 0.8),
+        height = math.floor(vim.o.lines * 0.8),
+        winblend = 10,
+        highlights = {
+          border = 'Normal',
+          background = 'Normal',
+        },
+      },
     })
 
-    -- Keybindings
+    -- Useful keybindings for different terminal layouts
+    local keymap = vim.keymap.set
+    local opts = { noremap = true, silent = true }
 
-    -- Normal mode: Toggle terminal
-    vim.api.nvim_set_keymap('n', '<C-\\>', '<cmd>ToggleTerm<CR>', { noremap = true, silent = true })
+    -- Main toggle
+    keymap('n', '<C-\\>', '<cmd>ToggleTerm<CR>', opts)
 
-    -- Terminal mode: Escape from terminal to normal mode
-    vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+    -- Terminal directions
+    keymap('n', '<leader>th', '<cmd>ToggleTerm direction=horizontal<CR>', { desc = 'Terminal horizontal' })
+    keymap('n', '<leader>tv', '<cmd>ToggleTerm direction=vertical<CR>', { desc = 'Terminal vertical' })
+    keymap('n', '<leader>tf', '<cmd>ToggleTerm direction=float<CR>', { desc = 'Terminal float' })
+
+    -- Multiple terminals
+    keymap('n', '<leader>1', '<cmd>1ToggleTerm<CR>', { desc = 'Terminal 1' })
+    keymap('n', '<leader>2', '<cmd>2ToggleTerm<CR>', { desc = 'Terminal 2' })
+    keymap('n', '<leader>3', '<cmd>3ToggleTerm<CR>', { desc = 'Terminal 3' })
+
+    -- Send to terminal
+    keymap('n', '<leader>ts', '<cmd>ToggleTermSendCurrentLine<CR>', { desc = 'Send line to terminal' })
+    keymap('v', '<leader>ts', '<cmd>ToggleTermSendVisualSelection<CR>', { desc = 'Send selection to terminal' })
+
+    -- Custom terminal functions
+    local Terminal = require('toggleterm.terminal').Terminal
+
+    -- Node REPL
+    local node = Terminal:new({ cmd = 'node', hidden = true })
+    function _NODE_TOGGLE()
+      node:toggle()
+    end
+    keymap('n', '<leader>tn', '<cmd>lua _NODE_TOGGLE()<CR>', vim.tbl_extend('force', opts, { desc = 'Node REPL' }))
+
+    -- Terminal management commands
+    keymap(
+      'n',
+      '<leader>ta',
+      '<cmd>ToggleTermToggleAll<CR>',
+      vim.tbl_extend('force', opts, { desc = 'Toggle all terminals' })
+    )
   end,
 }
