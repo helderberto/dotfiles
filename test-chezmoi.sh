3f707363489bdaeda5694a7549fd370ebed4d168
Helder Burato Berto <helder.burato@gmail.com>
2026-02-25 16:50:08 +0000

    fix: keep .tmpl extension for chezmoi config recognition

--- test-chezmoi.sh
+++ test-chezmoi.sh
@@ -66,7 +66,7 @@ REQUIRED_FILES=(
     "dot_config/ghostty/config"
     "dot_config/nvim/init.lua"
     ".chezmoiignore"
-    ".chezmoi.toml"
+    ".chezmoi.toml.tmpl"
 )
 for file in "${REQUIRED_FILES[@]}"; do
     if [ -e "$CHEZMOI_SOURCE/$file" ]; then
