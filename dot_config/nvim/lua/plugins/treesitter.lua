27355227ad5525b27d1af13cd3cd7d807b62df58
Helder Burato Berto <helder.burato@gmail.com>
2026-04-01 11:15:59 +0100

    fix: re-enable markdown treesitter highlight after TSUpdate

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -34,7 +34,7 @@ return {
       },
       highlight = {
         enable = true,
-        disable = { 'markdown', 'markdown_inline' },
+        disable = {},
       },
       indent = {
         enable = true,
